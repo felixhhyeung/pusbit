b0234f5bf20c3015a23fa0beb500929e68ec4f12
Felix HH Yeung <felixhhyeung@hotmail.com>
2024-05-04 23:06:58 +0800

    WIP

--- src/logger.ts
+++ src/logger.ts
@@ -2,16 +2,16 @@ import {
   createLogger,
   format,
   transports
-} from 'winston';
-import ConsoleLoggerTransport from './lib/winston-console-transport';
+} from "winston";
+import ConsoleLoggerTransport from "./lib/winston-console-transport";
 
 const logTransports = [
   new transports.File({
-    level: 'error',
-    filename: './logs/error.log',
+    level: "error",
+    filename: "./logs/error.log",
     format: format.json({
       replacer: (key, value) => {
-        if (key === 'error') {
+        if (key === "error") {
           return {
             message: (value as Error).message,
             stack: (value as Error).stack
@@ -29,8 +29,8 @@ const logger = createLogger({
     format.timestamp()
   ),
   transports: logTransports,
-  defaultMeta: { service: 'api' },
-  level: process.env.NODE_ENV === 'development' ? 'silly' : 'info'
+  defaultMeta: { service: "api" },
+  level: process.env.NODE_ENV === "development" ? "silly" : "info"
 });
 
 export default logger;
